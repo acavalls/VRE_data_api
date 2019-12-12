3622b360ffda2ee34a2a6c3dde07cad351c9c81e
root <root@EUSH-vre.vm.bsc.es>
2019-12-12 11:34:09 +0100

    moving documentation endpoint

--- app/controllers/StaticPagesController.php
+++ app/controllers/StaticPagesController.php
@@ -17,7 +17,7 @@ class StaticPagesController extends Controller {
 	
 	public function home($request, $response, $args) {
 		
-		$this->generateStaticPage($response, 'Hello, this is <strong>'.$this->global['shortProjectName'].'</strong> resource API. <br/>Local Repository: '.$this->global['local_repository'], 'home');
+		$this->generateStaticPage($response, 'Hello, this is <strong>'.$this->global['shortProjectName'].'</strong> resource API. <br/>Local Repository: '.$this->global['local_repository']."<br/>. Check the documentation at /doc<br/>", 'home');
 
 	}
 	
