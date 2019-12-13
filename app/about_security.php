1d3e2cfab24d463e2c20c2ac6dcd529f30dace65
acavalls <alejandro.canosa@bsc.es>
2019-12-13 16:01:40 +0100

    Swagger: Security docs modification

--- app/about_security.php
+++ app/about_security.php
@@ -2,17 +2,17 @@
 /**
  * @SWG\SecurityScheme(
  *   securityDefinition="bearer",
- *   description="This API expects users to authentificate via bearer tokens. Obtain them at http://multiscalegenomics.bsc.es,</p><p>1 - login using any of the configured OAuth2 providers and your token will be displayed at 'My Profile' &rarr; 'API Keys' section.</p><p>2 - Copy&paste it here in the following format: 'bearer access_token_string'",
+ *   description="This API expects users to authentificate via bearer tokens. Obtain them at https://vre.eucanshare.bsc.es,</p><p>1 - login using any of the configured OAuth2 providers and your token will be displayed at 'My Profile' &rarr; 'API Keys' section.</p><p>2 - Copy&paste it here in the following format: 'bearer access_token_string'",
  *   type="apiKey",
  *   name="Authorization",
  *   in="header"
  * )
  * @SWG\SecurityScheme(
- *   securityDefinition="mug_auth",
- *   description="This API expects users to authentificate via bearer tokens obtained via OAuth2. If you have an authorized OAuth2 client, you can generate them here. If not, open the OAuth2 dialog at http://multiscalegenomics.bsc.es to obtain them, as detailed in the above section<br/>",
+ *   securityDefinition="vre_auth",
+ *   description="This API expects users to authentificate via bearer tokens obtained via OAuth2. If you have an authorized OAuth2 client, you can generate them here. If not, open the OAuth2 dialog at http://vre.eucanshare.bsc.es to obtain them, as detailed in the above section<br/>",
  *   type="oauth2",
- *   authorizationUrl="https://inb.bsc.es/auth/realms/mug/protocol/openid-connect/auth",
- *   tokenUrl="https://inb.bsc.es/auth/realms/mug/protocol/openid-connect/token",
+ *   authorizationUrl="https://inb.bsc.es/auth/realms/eucanshare/protocol/openid-connect/userinfo",
+ *   tokenUrl="https://inb.bsc.es/auth/realms/eucanshare/protocol/openid-connect/token",
  *   flow="accessCode",
  *   scopes={
  *        "read" : "read your files",
